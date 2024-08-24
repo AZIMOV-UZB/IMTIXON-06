1414271f5ffd0139e2c0806f8d5a6ceeec5fec42
AZIMOV-UZB <abduvorisazimov787@gmail.com>
2024-08-24 13:20:42 -0700

    imtixon

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
 
   const products = (
     <div className="container  py-4 mt-10">
-      <ul className="category category flex whitespace-nowrap ul__text  overflow-x-auto scroll-none gap-4 ">
+      <ul className="category category container mx-auto px-5 flex whitespace-nowrap ul__text  overflow-x-auto scroll-none gap-4 ">
         <li>
           <button
             onClick={() => setCategory("")}
